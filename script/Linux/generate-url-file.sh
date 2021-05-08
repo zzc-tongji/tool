7c722faccd77c858b627573c90a0b0c72f585859
Zhicheng Zhang <20331014+zzc-tongji@users.noreply.github.com>
2021-05-09 07:07:25 +0800

    commit

--- script/Linux/generate-url-file.sh
+++ script/Linux/generate-url-file.sh
@@ -4,14 +4,14 @@ set -e
 SCRIPT_PATH="$( cd "$(dirname "$0")" ; pwd -P )"
 
 if [ -z "$1" ]; then
-	echo "URL not set, required."
-	exit 1
+  echo "URL not set, required."
+  exit 1
 fi
 
 if [ -z "$2" ]; then
-	echo '[InternetShortcut]'
-	echo "URL=$1"
+  echo '[InternetShortcut]'
+  echo "URL=$1"
 else
-	echo '[InternetShortcut]' > "$2"
-	echo "URL=$1" >> "$2"
+  echo '[InternetShortcut]' > "$2"
+  echo "URL=$1" >> "$2"
 fi
