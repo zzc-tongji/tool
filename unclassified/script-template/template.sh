e22f16068ffc592b07874c75b4ac1d2f827cde6a
Zhicheng Zhang <20331014+zzc-tongji@users.noreply.github.com>
2025-10-31 17:49:36 +0800

    commit

--- unclassified/script-template/template.sh
+++ unclassified/script-template/template.sh
@@ -1,8 +1,16 @@
 #!/bin/sh
 set -eux
 
+#
+# Add validation of input parameter(s) here.
+#
+if [ "$#" -lt 0 ]; then
+  echo "Usage: $0"
+  exit 1
+fi
+
 # start pwd & script dir
-START_PWD=${PWD}
+START_PWD="${PWD}"
 SHELL_NAME=`ps -p $$ | awk 'NR==2{print $4}'`
 if [ "${SHELL_NAME}" = "zsh" ]; then
   SCRIPT_DIR=`cd "$(dirname "${(%):-%N}")"; pwd -P`
@@ -13,17 +21,19 @@ else
 fi
 
 # finally
-function finally {
+finally () {
   #
   # Do something at the end of the script
   # regardless of success or failure.
   #
+  cd "${START_PWD}"
 }
 trap finally EXIT
 
 #
 # Do something.
 #
+cd "${SCRIPT_DIR}"
 
 # exit
 exit 0
